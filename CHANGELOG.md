d744178b9197e2ef5f0c279dab195e29c86699af
Alexeev Bronislav <alexeev.dev@mail.ru>
2025-06-25 03:01:56 +0700

    create structure

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+commit 68ba3c428b383821f451b8728927f4cf104e51e8
+Author: Alexeev Bronislav <alexeev.dev@mail.ru>
+Date:   Wed Jun 25 02:58:00 2025 +0700
+
+    Initial commit
---
+++ shell.nix
@@ -0,0 +1,40 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  libs = with pkgs; [
+    boost
+    cmake
+    clang
+    make
+    clang-tools
+    clang-tidy-sarif
+    clang-analyzer
+    gcc
+    lld
+    pkg-config
+    gdb
+    valgrind
+  ];
+  lib = libs;
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    cppcheck
+    clang-tools
+    codespell
+    conan
+    doxygen
+    lcov
+  ];
+  buildInputs = libs;
+  # XDG_DATA_DIRS = builtins.getEnv "XDG_DATA_DIRS";
+  # XDG_RUNTIME_DIRS = "/run/user/1001/";
+  shellHook = ''
+    echo "morning.lang Dev Shell"
+    # Ensure proper environment setup for GCC and glibc
+    # Correct the CXXFLAGS by removing `.dev` from the GCC path
+    export CXXFLAGS="-I${pkgs.gcc}/include/c++/${pkgs.gcc.version} -I${pkgs.glibc}/include"
+
+    export CC=clang
+    export CXX=clang++
+  '';
+}
